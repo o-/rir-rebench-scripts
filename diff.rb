650fac41c454862e2ea8fff88f860365c016ead2
oli <o@o1o.ch>
2019-04-17 14:45:02 +0200

    color output

--- diff.rb
+++ diff.rb
@@ -2,13 +2,20 @@
 
 require 'bigdecimal'
 
+class String
+  def red;            "\e[31m#{self}\e[0m" end
+  def green;          "\e[32m#{self}\e[0m" end
+  def gray;           "\e[37m#{self}\e[0m" end
+  def bold;           "\e[1m#{self}\e[22m" end
+end
+
 # https://stackoverflow.com/a/14744502
 def gmean(xs)
   one = BigDecimal.new 1
   xs.map { |x| BigDecimal.new x }.inject(one, :*) ** (one / xs.size)
 end
 
-WARMUP = 3
+WARMUP = 4
 
 def sum_up(results)
   (results.map{|r| Float(r)}.inject(:+) / results.size).to_i
@@ -41,6 +48,25 @@ b = read(ARGV[1])
 wdiffs = []
 diffs = []
 
+def format_number(speedup)
+  speedup_s = speedup.round(2).to_s('F')
+
+  diff = (speedup-1).abs
+  if diff > 0.1
+      speedup_s = speedup_s.bold
+  end
+
+  speedup_s = if speedup >= 1.04
+    speedup_s.green
+  elsif speedup <= 0.96
+    speedup_s.red
+  elsif diff <= 0.02
+    speedup_s.gray
+  else
+    speedup_s
+  end
+end
+
 b.each do |n, t|
   name = n
   (40-name.length).times{ name+= " " }
@@ -51,7 +77,10 @@ b.each do |n, t|
   wdiffs << speedup
   diffs << raw_speedup
 
-  puts "#{name} #{speedup.round(2).to_s('F')}\t(#{raw_speedup.round(2).to_s('F')})    \twu: #{t[0]}\tvs. #{c[n][0]}"
+  speedup_s = format_number(speedup)
+  raw_speedup_s = format_number(raw_speedup)
+
+  puts "#{name} #{speedup_s}\t(#{raw_speedup_s})    \twu: #{t[0]}\tvs. #{c[n][0]}"
 end
 
 puts "                                         #{gmean(wdiffs).round(2).to_s('F')}\t(#{gmean(diffs).round(2).to_s('F')})"
